41667c5893ca21d9d97dfacf4ec91875af29c905
Mattia Cerrato <mcerrato@uni-mainz.de>
2021-11-30 15:10:56 +0100

    Reorganize code

--- agents/VanillaGradMLP.py
+++ agents/VanillaGradMLP.py
@@ -1,7 +1,13 @@
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 from agents.initializers import xavier_init
+import torch
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
+import torch.distributions as distributions
+import numpy as np
+
 
 class VanillaGradMLP(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, dropout = 0.5,
@@ -25,4 +31,83 @@ class VanillaGradMLP(nn.Module):
     def init_weights(self):
         self.apply(xavier_init)
 
+    def train_episode(self, env, discount_factor):
+        self.train()
+        log_prob_actions = []
+        rewards = []
+        done = False
+        episode_reward = 0
+        state = env.reset()
+        while not done:
+            state = torch.FloatTensor(state).unsqueeze(0)
+            action_pred = self(state)
+            action_prob = F.softmax(action_pred, dim=-1)
+            dist = distributions.Categorical(action_prob)
+            action = dist.sample()  # todo: counter --> only choose actions every x iterations
+            log_prob_action = dist.log_prob(action)
+            state, reward, done, _ = env.step(action.item())
+            log_prob_actions.append(log_prob_action)
+            rewards.append(reward)
+            episode_reward += reward
+        log_prob_actions = torch.cat(log_prob_actions)
+        returns = self.calculate_returns(rewards, discount_factor)
+        loss = self.update_policy(returns, log_prob_actions, self.optimizer)
+        # print(loss,episode_reward)
+        return loss, episode_reward
 
+    def calculate_returns(self, rewards, discount_factor, normalize=True):
+        returns = []
+        R = 0
+        for r in reversed(rewards):
+            R = r + R * discount_factor
+            returns.insert(0, R)
+        returns = torch.tensor(returns)
+        if normalize:
+            returns = (returns - returns.mean()) / returns.std()
+        return returns
+
+    def update_policy(self, returns, log_prob_actions, optimizer):
+        returns = returns.detach()
+        loss = - (returns * log_prob_actions).sum()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        return loss.item()
+
+    def evaluate(self, env, policy):
+        policy.eval()
+        done = False
+        episode_reward = 0
+        state = env.reset()
+        while not done:
+            state = torch.FloatTensor(state).unsqueeze(0)
+            with torch.no_grad():
+                action_pred = policy(state)
+                action_prob = F.softmax(action_pred, dim=-1)
+            action = torch.argmax(action_prob, dim=-1)
+            state, reward, done, _ = env.step(action.item())
+            episode_reward += reward
+        return episode_reward
+
+    def train_loop(self, train_env, test_env, config):
+        MAX_EPISODES = config.episodes
+        DISCOUNT_FACTOR = config.discount
+        N_TRIALS = config.trials
+        REWARD_THRESHOLD = config.threshold
+        PRINT_EVERY = config.printevery
+        train_rewards = []
+        test_rewards = []
+        for episode in range(1, MAX_EPISODES + 1):
+            loss, train_reward = self.train_episode(train_env, DISCOUNT_FACTOR)
+            test_reward = self.evaluate(test_env)
+            train_rewards.append(train_reward)
+            test_rewards.append(test_reward)
+            mean_train_rewards = np.mean(train_rewards[-N_TRIALS:])
+            mean_test_rewards = np.mean(test_rewards[-N_TRIALS:])
+            if episode % PRINT_EVERY == 0:
+                print(
+                    f'| Episode: {episode:3} | Mean Train Rewards: {mean_train_rewards:5.1f} | Mean Test Rewards: {mean_test_rewards:5.1f} |')
+            if mean_test_rewards >= REWARD_THRESHOLD:
+                print(f'Reached reward threshold in {episode} episodes')
+                break
+        return train_rewards, test_rewards
--- run_agent.py
+++ run_agent.py
@@ -9,13 +9,6 @@ from collections import namedtuple, deque
 from itertools import count
 from PIL import Image
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torchvision.transforms as T
-import torch.distributions as distributions
-
 from environments.GymEnv import GymEnv
 from ScaleEnvironment.Scale import Scale
 from agents.VanillaGradMLP import VanillaGradMLP
@@ -35,93 +28,10 @@ def create_envs(env_str, seed=42):
     return train_env, test_env
 
 def get_env_dims(env):
-    return env.observation_space.shape[0], env.action_space.n
-
-
-def train_episode(env, policy, discount_factor):
-    policy.train()
-    log_prob_actions = []
-    rewards = []
-    done = False
-    episode_reward = 0
-    state = env.reset()
-    while not done:
-        state = torch.FloatTensor(state).unsqueeze(0)
-        action_pred = policy(state)
-        action_prob = F.softmax(action_pred, dim=-1)
-        dist = distributions.Categorical(action_prob)
-        action = dist.sample() # todo: counter --> only choose actions every x iterations
-        log_prob_action = dist.log_prob(action)
-        state, reward, done, _ = env.step(action.item())
-        log_prob_actions.append(log_prob_action)
-        rewards.append(reward)
-        episode_reward += reward
-    log_prob_actions = torch.cat(log_prob_actions)
-    returns = calculate_returns(rewards, discount_factor)
-    loss = update_policy(returns, log_prob_actions, policy.optimizer)
-    #print(loss,episode_reward)
-    return loss, episode_reward
-
-
-def calculate_returns(rewards, discount_factor, normalize=True):
-    returns = []
-    R = 0
-    for r in reversed(rewards):
-        R = r + R * discount_factor
-        returns.insert(0, R)
-    returns = torch.tensor(returns)
-    if normalize:
-        returns = (returns - returns.mean()) / returns.std()
-    return returns
-
-
-def update_policy(returns, log_prob_actions, optimizer):
-    returns = returns.detach()
-    loss = - (returns * log_prob_actions).sum()
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    return loss.item()
-
-
-def evaluate(env, policy):
-    policy.eval()
-    done = False
-    episode_reward = 0
-    state = env.reset()
-    while not done:
-        state = torch.FloatTensor(state).unsqueeze(0)
-        with torch.no_grad():
-            action_pred = policy(state)
-            action_prob = F.softmax(action_pred, dim=-1)
-        action = torch.argmax(action_prob, dim=-1)
-        state, reward, done, _ = env.step(action.item())
-        episode_reward += reward
-    return episode_reward
-
-def train_loop(agent, config):
-    MAX_EPISODES = config.episodes
-    DISCOUNT_FACTOR = config.discount
-    N_TRIALS = config.trials
-    REWARD_THRESHOLD = config.threshold
-    PRINT_EVERY = config.printevery
-    train_rewards = []
-    test_rewards = []
-    for episode in range(1, MAX_EPISODES + 1):
-        loss, train_reward = train_episode(train_env, agent, DISCOUNT_FACTOR)
-        test_reward = evaluate(test_env, agent)
-        train_rewards.append(train_reward)
-        test_rewards.append(test_reward)
-        mean_train_rewards = np.mean(train_rewards[-N_TRIALS:])
-        mean_test_rewards = np.mean(test_rewards[-N_TRIALS:])
-        if episode % PRINT_EVERY == 0:
-            print(
-                f'| Episode: {episode:3} | Mean Train Rewards: {mean_train_rewards:5.1f} | Mean Test Rewards: {mean_test_rewards:5.1f} |')
-        if mean_test_rewards >= REWARD_THRESHOLD:
-            print(f'Reached reward threshold in {episode} episodes')
-            break
-    return train_rewards, test_rewards
-
+    try:
+        return env.observation_space.shape[0], env.action_space.n
+    except TypeError: # if observation_space and action_space are dicts
+        return len(env.observation_space), len(env.action_space)
 
 def plot_rewards(train_rewards, test_rewards, threshold):
     plt.figure(figsize=(12,8))
@@ -145,8 +55,8 @@ if __name__ == '__main__':
     parser.add_argument('--threshold', type=float, default=475)
     args = parser.parse_args()
     train_env, test_env = create_envs(args.envname, seed=args.seed)
-    input_dim, output_dim = 5, 3 #get_env_dims(train_env)
+    input_dim, output_dim = get_env_dims(train_env)
     agent = VanillaGradMLP(input_dim, 100, output_dim)
-    mean_train_rewards, mean_test_rewards = train_loop(agent, args)
+    mean_train_rewards, mean_test_rewards = agent.train_loop(train_env, test_env, args)
     plot_rewards(mean_train_rewards, mean_test_rewards, args.threshold)
 
